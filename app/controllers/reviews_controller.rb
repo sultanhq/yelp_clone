2fe8c255d491a2f041b0c367bd8e5a6ae83ff0c3
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-11 09:07:24 +0000

    added authenticate_user function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
