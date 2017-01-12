514bce11b32cd5ba25af89f07d0903cd6d885eec
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-12 16:56:51 +0000

    Added average rating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -11,4 +11,12 @@ class Restaurant < ApplicationRecord
     review.user = user
     review
   end
+
+  def average_rating
+    output = 0
+    self.reviews.each do |review|
+      output += review.rating
+    end
+    output = output/self.reviews.count
+  end
 end
--- spec/features/reviews_features_spec.rb
+++ spec/features/reviews_features_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 feature 'reviewing' do
-
   scenario 'allows users to leave a review using a form' do
     sign_up
     add_a_restaurant
     visit '/restaurants'
     click_link 'Review KFC'
-    fill_in "Thoughts", with: "Blurgh"
+    fill_in 'Thoughts', with: 'Blurgh'
     select '3', from: 'Rating'
     click_button 'Leave Review'
     click_link 'KFC'
     expect(page).to have_content('Blurgh')
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    sign_up
+    add_a_restaurant
+    leave_review('meh', '1')
+    click_link('Sign out')
+    sign_up_2
+    leave_review('great', '5')
+    expect(page).to have_content('Average Rating : 3')
   end
 end
